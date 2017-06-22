ce585542a217afc56dcd2996e56fb1fb0e12f7b7
sumAlbert <982675088@qq.com>
2017-06-23 00:17:45 +0800

    some change by zq 6/22

--- src/sql/SqlSearch.java
+++ src/sql/SqlSearch.java
@@ -1,16 +1,22 @@
 package sql;
 
+import entity.Table;
+
 import static com.opensymphony.xwork2.Action.SUCCESS;
 
 /**
  * Created by dell2 on 2017/6/22.
  */
 public class SqlSearch {
+
     public String execute() throws Exception {
         // TODO Auto-generated method stub
         return SUCCESS;
     }
-    public void search(String tableName){
+    public Table search(String tableName){
+        Table table=new Table();
 
+
+        return table;
     }
 }
